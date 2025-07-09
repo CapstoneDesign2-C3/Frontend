a3b2cfc8084ff1bd9aec9b9f491d4093b2a6575a
2iedo <162278702+2iedo@users.noreply.github.com>
2025-07-09 15:24:03 +0900

    feat : 경로 추가, axios 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeLayout from './page/home/HomeLayout.tsx'
-import CameraLayout from './page/camera/CameraLayout.tsx';
+import HomePage from './page/home/HomePage.tsx'
+import CameraPage from './page/camera/CameraPage.tsx';
+import VideoDetailPage from './page/video/VideoDetailPage.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<HomeLayout />} />
-        <Route path="/camera" element={<CameraLayout />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/camera" element={<CameraPage />} />
+        <Route path="/videos/:id" element={<VideoDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
