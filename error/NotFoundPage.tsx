f61d826a4d6c77f2d670e5c4b4caaac806345e68
2iedo <dn06023@naver.com>
2025-07-30 09:44:49 +0900

    refactor : useRouter로 변경

--- error/NotFoundPage.tsx
+++ error/NotFoundPage.tsx
@@ -1,7 +1,7 @@
-import { useNavigate } from "react-router-dom";
+import { useRouter } from 'next/router';
 
 function NotFoundPage() {
-  const navigate = useNavigate();
+  const router = useRouter();
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -9,7 +9,7 @@ function NotFoundPage() {
       <p className="text-lg text-gray-600 mb-6">페이지를 찾을 수 없습니다.</p>
       <button
         className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-        onClick={() => navigate(-1)}
+        onClick={() => router.back()}  // ← 여기 수정
       >
         ← 이전 페이지로 이동
       </button>
