dd10d946141141fd32cb3b992ae30454d437b844
2iedo <dn06023@naver.com>
2025-08-14 09:19:35 +0900

    refactor : 카메라 페이지 변경

--- app/camera/page.tsx
+++ app/camera/page.tsx
@@ -15,35 +15,32 @@ function Camera(){
   }, []);
 
   return (
-    <div>
-      <div className="flex h-screen bg-gray-100">
-        <div className="flex-1 relative">
-          <GoogleMap onMarkerClick={selectCamera} />
+  <div className="flex h-full">
+    <GoogleMap onMarkerClick={selectCamera} />
+    {camera && (
+    <div className="w-[20%] flex flex-col h-full">
+      <div className="flex justify-between items-center">
+        <div className="flex-1">
+          <h2 className='text-xl'> {camera.cameraScenery}</h2>
+          <h2 className='text-xs'>위도 | {camera.cameraLatitude}</h2>
+          <h2 className='text-xs'>경도 | {camera.cameraLongitude}</h2>
         </div>
-        {camera && (<div className="w-[300px] bg-white border-l flex flex-col h-full">
-          <div className="flex justify-between items-center border">
-            <div className="flex-1">
-              <h2 className='text-xl'> {camera.cameraScenery}</h2>
-              <h2 className='text-xs'>위도 | {camera.cameraLatitude}</h2>
-              <h2 className='text-xs'>경도 | {camera.cameraLongitude}</h2>
-            </div>
-            <button className="text-gray-400 hover:text-gray-800 text-xl" onClick={() => selectCamera(null)}>
-            ×
-            </button>
-          </div>
-          <div className="overflow-y-auto">
-            {camera.videos && camera.videos.length > 0 ? (
-              camera.videos.map((video) => (
-                <EventForm key={video.videoId} video={video} />
-              ))
-            ) : (
-              <div className="text-gray-400 p-4">이 카메라에 연결된 이벤트가 없습니다.</div>
-            )}
-          </div>
-        </div>
+        <button className="hover:text-gray-300 text-xl" onClick={() => selectCamera(null)}>
+        ×
+        </button>
+      </div>
+      <div className="overflow-y-auto flex-1">
+        {camera.videos && camera.videos.length > 0 ? (
+          camera.videos.map((video) => (
+            <EventForm key={video.videoId} video={video} />
+          ))
+        ) : (
+          <div className="text-gray-400 p-4">이 카메라에 연결된 이벤트가 없습니다.</div>
         )}
       </div>
     </div>
+    )}
+  </div>
   );
 }
 
