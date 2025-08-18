2484e7a581d68a314bc452d85f35c584dd2c8e2e
2iedo <dn06023@naver.com>
2025-08-18 13:09:54 +0900

    refactor : 헤더 변경 및 사이드바 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
 import type { Metadata } from "next";
-import CommonHeader from "../components/header/Header";
+import SideBar from "../components/sidebar/Sidebar";
 import "./globals.css";
+import Header from "@/components/header/Header";
 
 export const metadata: Metadata = {
-  title: "Hiperwall",
+  title: "하이퍼월",
   description: "Control-Automation",
+  icons: {
+    icon: "/icons/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="ko" className="h-full w-full">
-      <body className="h-full w-full m-0 p-0 flex flex-col">
-        <CommonHeader />
-        {children}
+    <html lang="ko" className="h-screen w-full">
+      <body className="h-screen w-full flex-col">
+        <Header />
+        <main className="h-[95vh] w-full flex">
+          <SideBar />
+          {children}
+        </main>
       </body>
     </html>
   );
