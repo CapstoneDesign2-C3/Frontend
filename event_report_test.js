d4a8270c8ce15ee6b83c878c1374aa1af1606918
2iedo <dn06023@naver.com>
2025-08-22 11:28:08 +0900

    feat : 이벤트 보고서 생성 테스트코드 추가

---
+++ event_report_test.js
@@ -0,0 +1,35 @@
+// docker run --rm -it -v ${PWD}:/scripts -e K6_LOG_LEVEL=info grafana/k6 run /scripts/event_report_test.js
+
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    vus: 1,           // 동시 VU 1명만 실행
+    iterations: 50,   // 총 50번 실행
+};
+
+export default function () {
+    const url = 'http://3.35.16.8/api/v1/report/create-event';
+
+    const payload = JSON.stringify({
+        "startTime": "2025-07-21T00:00:00.00",
+        "endTime": "2025-07-21T23:59:59.99",
+        "author": "string"
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const response = http.post(url, payload, params);
+
+    check(response, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    console.log(`${__ITER + 1},${response.timings.duration}`);
+
+    sleep(1);
+}
